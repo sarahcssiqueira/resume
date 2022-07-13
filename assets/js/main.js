ddfc522948d24713fefeab7fa3869eee24a68e43
sarahcssiqueira <sarahcso@hotmail.com>
2022-07-13 14:05:43 -0300

    new collapsible buttoun 'read more'
    Signed-off-by: sarahcssiqueira <sarahcso@hotmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,20 @@ jQuery(document).ready(function($) {
 
 
 });
+
+//collapsible "Read More" button 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
